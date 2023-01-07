d43dbdcfdb442305107cc94598c84a370e5ce120
Rajeshkanna <rajeshkanna.muthalagar@learnship.com>
2023-01-07 16:24:07 +0530

    first commit with basic files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+var bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
